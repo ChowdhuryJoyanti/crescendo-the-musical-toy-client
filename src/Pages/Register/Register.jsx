c500c24756bf09d29495120a24834ebde739891b
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-05-19 17:22:35 +0600

    register added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,28 +2,30 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../provider/AuthProvider';
 
-const Login = () => {
-
-    const {signIn} = useContext(AuthContext);
-
+const Register = () => {
+    const {createUser} = useContext(AuthContext);
 
 
 
-    const handleLogIn = event => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password,);
-        signIn(email,password)
-        .then(result =>{
+
+    const handleRegister = event => {
+            event.preventDefault();
+            const form =event.target;
+            const name =form.name.value;
+            const email =form.email.value;
+            const password =form.password.value;
+            const photo =form.photo.value;
+         console.log(name,email,password,photo);
+
+
+
+         createUser(email,password)
+         .then(result =>{
             const user = result.user;
             console.log(user);
-        })
-        .catch(error => console.log(error));
+         })
+         .catch(error => console.log(error))
     }
-
-
     return (
         <div>
 
@@ -35,32 +37,46 @@ const Login = () => {
                         <div className="hero min-h-screen">
                             <div className="hero-content flex-col ">
                                 <div className="text-center lg:text-left">
-                                    <h1 className="text-5xl font-bold">Login now!</h1>
+                                    <h1 className="text-5xl font-bold">Register now!</h1>
                                 </div>
                                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                                     <div className="card-body">
-                                        <form onSubmit={handleLogIn}>
+                                        <form onSubmit={handleRegister}>
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text">Name</span>
+                                                </label>
+                                                <input type="text" name='name' placeholder="name" className="input input-bordered"  required/>
+                                            </div>
                                             <div className="form-control">
                                                 <label className="label">
                                                     <span className="label-text">Email</span>
                                                 </label>
-                                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                                                <input type="email" name='email' placeholder="email" className="input input-bordered" required/>
                                             </div>
                                             <div className="form-control">
                                                 <label className="label">
                                                     <span className="label-text">Password</span>
                                                 </label>
-                                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                                <input type="text" name='password' placeholder="password" className="input input-bordered" required />
+                                                
+                                            </div>
+
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text">PhotoURL</span>
+                                                </label>
+                                                <input type="text" name='photo' placeholder="PhotoURL" className="input input-bordered" />
                                                 <label className="label">
                                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                                 </label>
                                             </div>
                                             <div className="form-control mt-6">
 
-                                                <input className="btn btn-warning" type="submit" value="Login" />
+                                                <input className="btn btn-warning" type="submit" value="Register" />
                                             </div>
                                         </form>
-                                        <p>Don't Have an account <Link className='text-yellow-600' to="/register">Register</Link></p>
+                                        <p> Have an account ?<Link className='text-yellow-600' to="/login">Login</Link></p>
                                     </div>
                                 </div>
                             </div>
@@ -109,4 +125,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Register;
