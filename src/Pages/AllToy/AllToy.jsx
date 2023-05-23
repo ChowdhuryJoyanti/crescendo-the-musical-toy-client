00ca86e18ac4bfe3f82a3407096334a5a7bfba4c
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-05-23 13:31:22 +0600

    search text correction

--- src/Pages/AllToy/AllToy.jsx
+++ src/Pages/AllToy/AllToy.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import ToyCard from '../ToyCard/ToyCard';
 
@@ -6,6 +6,16 @@ import ToyCard from '../ToyCard/ToyCard';
 const AllToy = () => {
 
   const toys = useLoaderData();
+  const [search, setSearch] = useState('');
+  const [searchData, setSearchData] = useState();
+
+  const handleSearch = (event) => {
+        fetch(`http://localhost:5000/searchToys?text=${search}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+
+  }
 
 
 
@@ -16,6 +26,43 @@ const AllToy = () => {
       <div>
         <div className='mt-5'>
           <h2 className='text-5xl text-center text-gray-300 font-bold'>All Toys</h2>
+
+
+          <div className="form-control">
+                <div className="input-group">
+                    <input type="text" placeholder="Search…" className="input input-bordered"   value={search}
+                   onChange={(event) => setSearch(event.target.value)}/>
+                   {/* <p>Search query: {searchQuery}</p> */}
+                      {
+                       
+                      }
+
+
+                    <button className="btn btn-square" onClick={handleSearch}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                </div>
+                
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                <thead>
+                        <tr>
+                            <th>
+                                {/* <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label> */}
+                            </th>
+                            <th> Toy Name</th>
+                            <th> Sub Category</th>
+                            <th>Price</th>
+                            <th>Available Quantity</th>
+                            <th>Seller Name</th>
+                            <th>Details</th>
+                        </tr>
+                    </thead>
+                    </table>
+                    </div>
+            </div>
 
         </div>
 
--- src/Pages/ToyCard/ToyCard.jsx
+++ src/Pages/ToyCard/ToyCard.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 // import React, { useState } from 'react';
-import Search from './Search';
+// import Search from './Search';
 
 const ToyCard = ({toy}) => {
 
@@ -11,12 +11,6 @@ const ToyCard = ({toy}) => {
     const { name, price, seller, email, category, subCategory, details, quantity, rating, photo } = toy;
 
 
-    const [searchQuery, setSearchQuery] = useState('');
-
-  const handleSearch = (event) => {
-    setSearchQuery(event.target.value);
-
-  }
 
 
     return (
@@ -114,34 +108,21 @@ const ToyCard = ({toy}) => {
 
 
 
-            <div className="form-control">
-                <div className="input-group">
-                    <input type="text" placeholder="Search…" className="input input-bordered"   value={searchQuery}
-        onChange={handleSearch}/>
-                    <button className="btn btn-square">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                    </button>
-                </div>
-            </div>
-
+        
 
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
                     {/* head */}
                     <thead>
-                        <tr>
-                            <th>
-                                {/* <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label> */}
-                            </th>
+                        {/* <tr>
+                    
                             <th> Toy Name</th>
                             <th> Sub Category</th>
                             <th>Price</th>
                             <th>Available Quantity</th>
                             <th>Seller Name</th>
                             <th>Details</th>
-                        </tr>
+                        </tr> */}
                     </thead>
                     <tbody>
                         {/* row 1 */}
